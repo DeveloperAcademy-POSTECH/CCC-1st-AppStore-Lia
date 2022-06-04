d761c8e4b133c2bcb8922e39e3cabf5d0c7816ff
Lia316 <kjk0825@postech.ac.kr>
2022-06-04 14:04:54 +0900

    feat✨ : set navigation title & top description (#4)

--- AppStore/AppStore/ViewController.swift
+++ AppStore/AppStore/ViewController.swift
@@ -11,6 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.view.backgroundColor = .black
+    }
+
+    private func setNavigationBar() {
+        self.navigationItem.title = "투데이"
+        self.navigationController?.navigationBar.barStyle = .black
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+        let dateLabel = UILabel()
+        dateLabel.text = "\n6월 4일 토요일" // 줄바꿈 - navigation.title과 더 가깝게 보내기 위함
+        dateLabel.numberOfLines = 2
+        dateLabel.textColor = .lightGray
+        dateLabel.font = UIFont.systemFont(ofSize: 14)
+
+        let dateBarButton = UIBarButtonItem(customView: dateLabel)
+
+        self.navigationItem.leftBarButtonItem = dateBarButton
     }
 
 }
