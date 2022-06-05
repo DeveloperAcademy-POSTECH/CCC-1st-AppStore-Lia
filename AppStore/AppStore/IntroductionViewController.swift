f2e594eb79c0992d0a4231998f5f1713252d9bdb
Lia316 <kjk0825@postech.ac.kr>
2022-06-05 19:52:34 +0900

    feat✨ : add new file for subviews (#7)

---
+++ AppStore/AppStore/IntroductionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IntroductionViewController.swift
+//  AppStore
+//
+//  Created by 리아 on 2022/06/05.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
