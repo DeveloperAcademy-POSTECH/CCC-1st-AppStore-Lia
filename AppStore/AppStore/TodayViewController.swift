a215ece117da1049b40d7ffffdec563f76d8f489
Lia316 <kjk0825@postech.ac.kr>
2022-06-09 00:45:10 +0900

    fix🐞 : review - seperate function into 3 piece (#6)

--- AppStore/AppStore/TodayViewController.swift
+++ AppStore/AppStore/TodayViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class TodayViewController: UIViewController {
 
+    lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "\n6월 4일 토요일" // 줄바꿈 - navigation.title과 더 가깝게 보내기 위함
+        label.numberOfLines = 2
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        return label
+    }()
+
+    lazy var profileButton: UIButton = {
+        let button = UIButton()
+        button.frame.size = CGSize(width: 35, height: 35)
+        button.setImage(UIImage(named: "LiaProfile"), for: .normal) // Object Literal Violation Warning...
+        button.layer.cornerRadius = button.frame.size.width / 2
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,29 +40,12 @@ class TodayViewController: UIViewController {
         self.navigationController?.navigationBar.barStyle = .black
         self.navigationController?.navigationBar.prefersLargeTitles = true
 
-        lazy var dateLabel: UILabel = {
-            let label = UILabel()
-            label.text = "\n6월 4일 토요일" // 줄바꿈 - navigation.title과 더 가깝게 보내기 위함
-            label.numberOfLines = 2
-            label.textColor = .lightGray
-            label.font = UIFont.systemFont(ofSize: 14)
-            return label
-        }()
-
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: dateLabel)
+        self.navigationItem.titleView = profileButton
+        setNavigationConstraint()
+    }
 
-        lazy var profileButton: UIButton = {
-            let button = UIButton()
-            button.frame.size = CGSize(width: 35, height: 35)
-            button.setImage(UIImage(named: "LiaProfile"), for: .normal) // Object Literal Violation Warning...
-            button.layer.cornerRadius = button.frame.size.width / 2
-            button.clipsToBounds = true
-            button.translatesAutoresizingMaskIntoConstraints = false
-            return button
-        }()
-
-        self.navigationItem.titleView = profileButton // profileImageView
-
+    private func setNavigationConstraint() {
         NSLayoutConstraint.activate([
             profileButton.trailingAnchor.constraint(equalTo: navigationController!.navigationBar.trailingAnchor,
                                                     constant: -13),
